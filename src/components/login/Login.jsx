f1f79714a4bfd122ad83caf36af6a1fd6eb07863
Gyanve <gyanvesharma@gmail.com>
2023-08-26 21:53:26 +0530

    initial-commit

--- src/components/login/Login.jsx
+++ src/components/login/Login.jsx
@@ -1,14 +1,19 @@
 import React, { useState } from "react";
-import { useRef, UseState, useEffect } from "react";
+import { useRef, useEffect, useContext } from "react";
+import axios from "../../api/axios";
 import "./LoginStyle.css";
+import AuthContext from "../../context/AuthProvider";
+
+const LOGIN_URL = "https://jsonplaceholder.typicode.com/users";
 
 const Login = () => {
+  const { setAuth } = useContext(AuthContext);
   const userRef = useRef();
   const errRef = useRef();
 
-  const [user, setUser] = useState('');
-  const [pwd, setPwd] = useState('');
-  const [errMsg, setErrMsg] = useState('');
+  const [user, setUser] = useState("");
+  const [pwd, setPwd] = useState("");
+  const [errMsg, setErrMsg] = useState("");
   const [success, setSuccess] = useState(false);
 
   useEffect(() => {
@@ -16,70 +21,97 @@ const Login = () => {
   }, []);
 
   useEffect(() => {
-    setErrMsg('');
-  }, [user, pwd])
+    setErrMsg("");
+  }, [user, pwd]);
 
   const handleSubmit = async (e) => {
     e.preventDefault();
-    setUser("");
-    setPwd("");
-    setSuccess(true);
+
+    try {
+      const response = await axios.post(
+        LOGIN_URL,
+        JSON.stringify({ user, pwd }),
+        {
+          headers: { "Content-type": "application.json" },
+          withCredentials: true,
+        }
+      );
+      console.log(JSON.stringify(response?.data));
+      // console.log(JSON.stringify(response ));
+      const accessToken = response?.data?.accessToken;
+      const roles = response?.data?.roles;
+      setAuth({ user, pwd, roles, accessToken });
+      setUser("");
+      setPwd("");
+      setSuccess(true);
+    } catch (err) {
+      if (!err?.response) {
+        setErrMsg("No Server Response");
+      } else if (err.response?.status === 400) {
+        setErrMsg("Missing Username or Passowrd ");
+      } else if (err.response?.status === 401) {
+        setErrMsg("Unauthorized");
+      } else {
+        setErrMsg("Login Failed");
+      }
+      errRef.current.focus();
+    }
   };
   return (
     <>
-      {success ? (
-        <section>
-          <h1>You Are Logged In</h1>
-          <br />
-          <p>
-            <a href="#"> Go To Home</a>
-          </p>
-        </section>
-      ) : (
-        <section>
-          <p
-            ref={errRef}
-            className={errMsg ? "errmsg" : "offscreen"}
-            aria-live="assertive"
-          >
-            {errMsg}
-          </p>
-
-          
+      <div id="main">
+        {success ? (
+          <section>
+            <div id="logged">
+              <h1>You Are Logged In </h1>
+              <br />
+              <p>
+                <a href="#a"> Go To Home</a>
+              </p>
+            </div>
+          </section>
+        ) : (
+          <section>
+            <p
+              ref={errRef}
+              className={errMsg ? "errmsg" : "offscreen"}
+              aria-live="assertive"
+            >
+              {errMsg}
+            </p>
             <div className="heading">
               <h1>LOGIN</h1>
             </div>
-
-            
-                <form onSubmit={handleSubmit}>
-                  <label htmlFor="username">Your Name </label>
-                  <input
-                    id="username"
-                    type="text"
-                    placeholder="Email or Username"
-                    ref={useRef}
-                    autoComplete="off"
-                    onChange={(e) => {
-                      setUser(e.target.value);
-                    }}
-                    value={user}
-                    required
-                  />
-                  <label htmlFor="password">Password : </label>
-                  <input
-                    id="password"
-                    type="password"
-                    placeholder="Email or password"
-                    onChange={(e) => {
-                      setPwd(e.target.value);
-                    }}
-                    value={pwd}
-                    required
-                  />
-                  <button className="btn"> Sign In </button>
-                </form>
-              </section>
-      )}
+            <form className="form" onSubmit={handleSubmit}>
+              <label htmlFor="username">Your Name: </label>
+              <input
+                id="username"
+                type="text"
+                placeholder="Email or Username"
+                ref={userRef}
+                autoComplete="off"
+                onChange={(e) => {
+                  setUser(e.target.value);
+                }}
+                value={user}
+                required
+              />
+              <label htmlFor="password">Password : </label>
+              <input
+                id="password"
+                type="password"
+                placeholder="Password"
+                onChange={(e) => {
+                  setPwd(e.target.value);
+                }}
+                value={pwd}
+                required
+              />
+              <button className="btn"> Sign In </button>
+            </form>
+          </section>
+        )}
+      </div>
     </>
   );
 };
